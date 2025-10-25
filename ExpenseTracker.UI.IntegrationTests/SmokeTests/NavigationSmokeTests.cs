f3f6403a010835aa4af457c9bd091c3dd83b4a23
EssentialProgrammingKnowledge <kamil.wojtasinski.poczta@gmail.com>
2025-10-25 16:38:59 +0200

    [ExpenseTracker] [M5] [T1] Solution

---
+++ ExpenseTracker.UI.IntegrationTests/SmokeTests/NavigationSmokeTests.cs
@@ -0,0 +1,49 @@
+﻿using ExpenseTracker.IntegrationTests.Setup;
+using ExpenseTracker.UI.IntegrationTests.Components;
+using Shouldly;
+
+namespace ExpenseTracker.UI.IntegrationTests.SmokeTests
+{
+    public class NavigationSmokeTests
+    {
+        private readonly TestFixture _testFixture = new();
+
+        [Theory]
+        [InlineData("/", "/login")]
+        [InlineData("/categories", "/login")]
+        [InlineData("/profile", "/login")]
+        public void NavigationTest_UnauthenticatedAccess_ShouldRedirectToLogin(string path, string expectedPath)
+        {
+            // Act
+            _testFixture.NavigateTo(path);
+
+            // Assert
+            _testFixture.CurrentUrl.ShouldContain(expectedPath);
+        }
+
+        [Fact]
+        public void NavigationTest_UnauthenticatedAccess_ShouldHideNavMenu()
+        {
+            // Act
+            _testFixture.NavigateTo("/categories");
+
+            // Assert
+            var navMenu = new NavMenuComponent(_testFixture.AppComponent, _testFixture.NavigateTo);
+            navMenu.IsVisible.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void NavigationTest_UnauthenticatedAccess_ShouldHideMostOfActionOfNavbar()
+        {
+            // Act
+            _testFixture.NavigateTo("/categories");
+
+            // Assert
+            var navbar = new NavbarComponent(_testFixture.AppComponent);
+            navbar.DisplayedUserNameTooltip.ShouldBeNull();
+            navbar.IsLogoutVisible.ShouldBeFalse();
+            navbar.IsProfileVisible.ShouldBeFalse();
+            navbar.ProfileTooltipText.ShouldBeNull();
+        }
+    }
+}
